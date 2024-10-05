714779164ad2430b3b200d24719cb8a69b76d4ea
“GabrielVScalese” <gabriel.scalese@hotmail.com>
2024-10-05 12:56:43 -0300

    feat: create schedule struct

---
+++ include/schedule_utils.h
@@ -0,0 +1,48 @@
+#ifndef SCHEDULE_UTILS_H
+#define SCHEDULE_UTILS_H
+
+/**
+ * Define tamanho maximo da string nome do tutor
+ */
+#define TUTOR_NAME_LENGTH 100
+
+/**
+ * Define tamanho maximo da string nome do pet
+ */
+#define PET_NAME_LENGTH 100
+
+/**
+ * Define tamanho maximo da string endereco do cliente
+ */
+#define ADDRESS_LENGTH 30
+
+/**
+ * Define tamanho maximo da string horario
+ */
+#define HORARY_LENGTH 5
+
+/**
+ * Define tamanho maximo da string telefone
+ */
+#define TELEPHONE_LENGTH 11
+
+/**
+ * Define tamanho maximo da string raca
+ */
+#define BREED_LENGTH 30
+
+/**
+ * Estrutura para armazenar dados uteis de um agendamento por parte do pet sitter
+ */
+typedef struct schedule {
+ char tutor_name[TUTOR_NAME_LENGTH];
+ char pet_name[PET_NAME_LENGTH];
+ char address[ADDRESS_LENGTH];
+ int number;
+ char telephone[TELEPHONE_LENGTH];
+ char breed[BREED_LENGTH];
+ char horary[HORARY_LENGTH];
+ int time;
+} schedule;
+
+#endif //SCHEDULE_UTILS_H
